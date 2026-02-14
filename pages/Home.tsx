35962d58c8ad4dd96d6d19eeec0bdac39a003eb0
Rohan Shrestha <103275068+sthar2820@users.noreply.github.com>
2026-02-14 15:23:52 -0600

    Refactor code structure for improved readability and maintainability

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -5,7 +5,7 @@ import { trackResumeView, trackResumeDownload, trackExternalLink } from '../util
 
 const Home = () => {
   const [showResume, setShowResume] = useState(false);
-  const resumePath = '/input/Rohan_Shrestha__Resume.pdf';
+  const resumePath = '/input/Rohan_Shrestha_Resume_DA.pdf';
 
   const handleResumeOpen = () => {
     trackResumeView();
